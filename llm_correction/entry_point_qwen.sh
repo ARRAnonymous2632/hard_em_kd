10d3e1448e3be58e7ea9787625432602b8426cf3
ARRAnonymous2632 <your.email@example.com>
2025-02-24 01:34:44 +0000

    update

--- llm_correction/entry_point_qwen.sh
+++ llm_correction/entry_point_qwen.sh
@@ -6,11 +6,11 @@ hypos=$2
 targets=$3
 sources=$4
 EXPERIMENT_DIR=$5
-start_idx=${6:-0}
-end_idx=${7:-1}
-use_targets=${8:-0}
-num_gpus=${9:-1}
-seed=${10:-6547}
+seed=${6:-6547}
+start_idx=${7:-0}
+end_idx=${8:-1}
+use_targets=${9:-0}
+num_gpus=${10:-1}
 dt=${11:-0}
 
 eval python qwen_batch_inference.py \
